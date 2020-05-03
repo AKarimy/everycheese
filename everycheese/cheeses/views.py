08856b5377e1dc763b921146fccab20396591a65
Arman Kariky <watchdogs2os3@gmail.com>
2020-05-03 17:42:04 +0430

    Implement cheese detail page

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -4,3 +4,8 @@ from .models import Cheese
 class CheeseListView(ListView):
     """Class docstring"""
     model = Cheese
+
+
+class CheeseDetailView(DetailView):
+    """Class docstring"""
+    model = Cheese
